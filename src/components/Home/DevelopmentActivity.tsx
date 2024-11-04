5c59c66ee849b8fa8b8aaffeef2f5580fec724f3
Pappyjay23 <pjinadu02@gmail.com>
2024-11-04 10:46:56 +0100

    feat: Added dyanmic user data to development activity section

--- src/components/Home/DevelopmentActivity.tsx
+++ src/components/Home/DevelopmentActivity.tsx
@@ -3,11 +3,13 @@ import Profile1 from "../../assets/profile2.svg";
 import Profile2 from "../../assets/profile3.svg";
 import { MdOutlineDeleteOutline } from "react-icons/md";
 import { Area, AreaChart, ResponsiveContainer, Tooltip } from "recharts";
+import { useAppSelector } from "../../store/store";
 const DevelopmentActivity = () => {
+	const user = useAppSelector((state) => state.auth.user);
 	const commits = [
 		{
-			user: "Ronald Bradley",
-			avatar: Profile,
+			user: user?.displayName,
+			avatar: user?.photoURL || Profile,
 			message: "Initial commit",
 			date: "April 15, 2018",
 		},
@@ -106,11 +108,13 @@ const DevelopmentActivity = () => {
 					key={i}
 					className='flex gap-3 items-center py-3 px-4 md:px-6 text-[60%] md:text-[70%] text-[#585F66] border-b border-[#d5d7de] font-medium'>
 					<div className='flex-1 flex items-center gap-2 md:gap-4'>
-						<img
-							src={commit.avatar}
-							alt='Profile img'
-							className='w-[25px] h-[25px] md:w-[30px] md:h-[30px] rounded-full object-cover'
-						/>
+						<div className="w-[25px] h-[25px] md:w-[30px] md:h-[30px] rounded-full overflow-hidden">
+							<img
+								src={commit.avatar}
+								alt='Profile img'
+								className='w-full h-full object-cover'
+							/>
+						</div>
 						<span>{commit.user}</span>
 					</div>
 					<p className='flex-1'>{commit.message}</p>
